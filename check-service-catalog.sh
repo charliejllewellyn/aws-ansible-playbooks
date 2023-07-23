4b0b5a55bd07078c37e861615469a10f12a43e63
Charlie Llewellyn <cjl@amazon.co.uk>
2023-07-23 07:35:13 +0100

    updated config

--- check-service-catalog.sh
+++ check-service-catalog.sh
@@ -1,12 +1,21 @@
 for run in {1..10}; do
-  productName=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-  aws servicecatalog provision-product --product-id prod-atlpq6fmtowqc --provisioning-artifact-id pa-y2aq32ikehjvu --provisioned-product-name $productName &>/dev/null
+  productName=k8-$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+  echo $productName
+  aws servicecatalog provision-product --product-id prod-atlpq6fmtowqc --provisioning-artifact-id pa-n5rtuy5zhduaa --provisioned-product-name $productName &>/dev/null
   
   while aws servicecatalog describe-provisioned-product --name $productName | jq .ProvisionedProductDetail.Status -r | grep UNDER_CHANGE &>/dev/null; do
-  sleep 10
+    sleep 10
   done
   
-  aws servicecatalog describe-provisioned-product --name $productName | jq .ProvisionedProductDetail.Status -r
+  status=$(aws servicecatalog describe-provisioned-product --name $productName | jq .ProvisionedProductDetail.Status -r)
+
+  echo $status
   
-  aws servicecatalog terminate-provisioned-product --provisioned-product-name $productName
+  if [[ "$status" == "AVAILABLE" ]]; then
+    s3Bucket=$(aws servicecatalog get-provisioned-product-outputs --provisioned-product-name $productName | jq .Outputs[1].OutputValue -r)
+  
+    aws servicecatalog terminate-provisioned-product --provisioned-product-name $productName &>/dev/null
+    aws s3 rm s3://$s3Bucket --recursive &>/dev/null
+    aws s3api delete-bucket --bucket $s3Bucket
+  fi
 done
